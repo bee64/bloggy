cbc49b1473635992b85b8a816167c14bb341a30c
Brad Smith <bradleys@hpe.com>
2017-05-25 16:31:39 -0600

    Finished auth service (Using thinkster API)

--- src/js/auth/index.js
+++ src/js/auth/index.js
@@ -2,7 +2,12 @@ import angular from 'angular';
 
 let authModule = angular.module('app.auth', []);
 
+// Import Config
 import AuthConfig from './auth.config.js';
 authModule.config(AuthConfig);
+
+// Import Controller
+import AuthCtrl from './auth.controller.js';
+authModule.controller('AuthCtrl', AuthCtrl);
 
 export default authModule;
