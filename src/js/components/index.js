715e1bf1bd4e46e309f757f575f57ef72d0bb8ac
Brad Smith <bradleys@hpe.com>
2017-05-26 11:52:51 -0600

    Finished show authed directive

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,5 +2,11 @@ import angular from 'angular';
 
 let componentsModule = angular.module('app.components', []);
 
+// Import components
+import ListErrors from './list-errors.component.js';
+componentsModule.component('listErrors', ListErrors);
+
+import ShowAuthed from './show-authed.directive.js';
+componentsModule.directive('showAuthed', ShowAuthed);
 
 export default componentsModule;
