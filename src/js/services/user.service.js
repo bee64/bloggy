fce6e2990de634d32a9404ff8635bdd7d84745e1
Brad Smith <bradleys@hpe.com>
2017-05-30 15:07:04 -0600

    Add JWT token to users

--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,9 +1,12 @@
 export default class User {
-	constructor(AppConstants, $http) {
+	constructor(AppConstants, JWT,  $http, $state, $q) {
 		'ngInject';
 
 		this._AppConstants = AppConstants;
+		this._JWT = JWT;
 		this._$http = $http;
+		this._$state = $state;
+		this._$q = $q;
 
 		// Information about the current user
 		this.current = null;
@@ -21,9 +24,48 @@ export default class User {
 		.then(
 			(res) => {
 				// Store user info after login
-				this.current = res.data.user
+				this.current = res.data.user;
+				this._JWT.save(this.current.token);
 				return res;
 			}
 		);
+	}
+
+	verifyAuth() {
+		let deferred = this._$q.defer();
+		if(!this._JWT.get()) {
+			deferred.resolve(false);
+			return deferred.promise;
+		}
+
+		if(this.current) {
+			// JWT token && User exist
+			deferred.resolve(true);
+		} else {
+			this._$http({
+				url: this._AppConstants.api + '/user',
+				method: 'GET',
+				headers: {
+					Authorization: 'Token ' + this._JWT.get()
+				}
+			})
+			.then(
+			(res) => {
+				this.current = res.data.user;
+				deferred.resolve(true);
+			},
+			(err) => {
+				this._JWT.destroy();
+				deferred.resolve(false);
+			});
+		}
+		return deferred.promise;
+	}
+
+	logOut() {
+		this.current = null;
+		this._JWT.destroy();
+		// Hard state reload
+		$state.go($state.current, {}, {reload: true});
 	}
 }
